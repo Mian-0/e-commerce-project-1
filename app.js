91a78922f709436f3c8e8cffd4e374bba897cd3d
Mian-0 <102787006+Mian-0@users.noreply.github.com>
2023-02-16 05:26:08 +0000

    Modal Completed

--- app.js
+++ app.js
@@ -121,5 +121,18 @@ currentProductSizes.forEach((size, index) => {
     })
     size.style.backgroundColor = "black"
     size.style.color = "white"
-  })
-})
+  });
+});
+
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
